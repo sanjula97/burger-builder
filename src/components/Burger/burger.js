349c25f8361bd677b25eab26eed96b02fed07d70
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-06-21 12:16:13 +0530

    Updated burger builder to add dynomic burger ingrediants

--- src/components/Burger/burger.js
+++ src/components/Burger/burger.js
@@ -3,11 +3,18 @@ import classes from './burger.module.css'
 import BurgerIngrediant from './BurgerIngrediant/burgerIngrediant'
 
 const burger = (props) => {
+
+    const transformedIngrediants = Object.keys(props.ingrediants)
+    .map(igKey =>{
+        return [...Array(props.ingrediants[igKey])].map((_,i) => {
+            return <BurgerIngrediant key={igKey + i} type={igKey} />
+        });
+    });
+    
     return(
         <div className={classes.Burger}>
             <BurgerIngrediant type="bread-top"/>
-            <BurgerIngrediant type="cheese"/>
-            <BurgerIngrediant type="meat"/>
+            {transformedIngrediants}
             <BurgerIngrediant type="bread-bottom"/>
         </div>
     );
