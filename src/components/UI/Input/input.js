0f682728838c5e1650cd4b9eb737b8acbfe13c87
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-07-11 16:41:16 +0530

    updated the form in caontact data

--- src/components/UI/Input/input.js
+++ src/components/UI/Input/input.js
@@ -4,15 +4,38 @@ import classes from './input.module.css';
 const Input = (props) => {
      let inputElement = null;
 
-    switch (props.inputtype) {
+    switch (props.elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...props} />
+            inputElement = <input 
+                            className={classes.InputElement} 
+                            {...props.elementConfig} 
+                            value={props.value} />
             break;
         case ('textarea'):
-            inputElement = <textarea className={classes.InputElement} {...props} />
+            inputElement = <textarea 
+                            className={classes.InputElement} 
+                            {...props.elementConfig} 
+                            value={props.value}/>
+            break;
+        case ('select'):
+            inputElement = (
+                        <select 
+                            className={classes.InputElement} 
+                            value={props.value}>
+                            {props.elementConfig.options.map(option => (
+                                <option value={option.value}
+                                        key={option.value}  >
+                                    {option.displayValue}
+                                </option>
+                            ))}
+                        </select>
+                    );
             break;
         default:
-            inputElement = <input className={classes.InputElement} {...props} />
+            inputElement = <input 
+                            className={classes.InputElement} 
+                            {...props.elementConfig} 
+                            value={props.value}/>
     }
 
     return(
--- src/containers/checkout/contactData/contactData.js
+++ src/containers/checkout/contactData/contactData.js
@@ -9,12 +9,58 @@ import Input from '../../../components/UI/Input/input'
 
 class ContactData extends Component {
     state={
-        name:'',
-        email:'',
-        address:{
-            street:'',
-            postalCode:''
-        },
+        orderForm:{
+            name:{
+                elementType:'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Your name'
+                },
+                value:''
+            },
+            street:{
+                elementType:'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Street'
+                },
+                value:''
+            },
+            zipcode:{
+                elementType:'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Zipcode'
+                },
+                value:''
+            },
+            country:{
+                elementType:'input',
+                elementConfig:{
+                    type:'text',
+                    placeholder:'Country'
+                },
+                value:''
+            },
+            email:{
+                elementType:'input',
+                elementConfig:{
+                    type:'email',
+                    placeholder:'Your Email'
+                },
+                value:''
+            },
+            delivaryMethod:{
+                elementType:'select',
+                elementConfig:{
+                    options: [
+                        {value:'fastest',displayValue:'Fastest'},
+                        {value:'cheapest',displayValue:'Cheapest'}
+                    ]
+                },
+                value:''
+            } 
+        },  
         loading:false
     }
     orderHandler = (event) => {
@@ -23,15 +69,7 @@ class ContactData extends Component {
         this.setState({loading:true});
         const order = {
             ingrediants: this.props.ingrediants,
-            price:this.props.price,
-            customer: {
-                name:'Sanjula karunarathna',
-                address:'Kurunegala',
-                zipcode:'6969',
-                country:'Germony'
-            },
-            email:'sanjula@test.com',
-            delivaryMethod:'fastest'
+            price:this.props.price
         }
         axios.post('/orders.json',order)
         .then(response => {
@@ -44,12 +82,22 @@ class ContactData extends Component {
     }
 
     render() {
+        const formElementsArray = [];
+        for(let key in this.state.orderForm){
+            formElementsArray.push({
+                id:key,
+                config: this.state.orderForm[key]
+            });
+        };
         let form =(
             <form>
-                <Input inputtype="input" type="text" name="name" placeholder="Your name"/>
-                <Input inputtype="input" type="email" name="email" placeholder="Your Mail"/>
-                <Input inputtype="input" type="text" name="street" placeholder="Your Street"/>
-                <Input inputtype="input" type="text" name="postal" placeholder="Postal code"/>
+                {formElementsArray.map(formElement => (
+                    <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}/>
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
