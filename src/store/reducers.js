82ed475218a40962e2f948bbd2114c61398cae40
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-07-26 00:33:57 +0530

    Added redux store to the project

---
+++ src/store/reducers.js
@@ -0,0 +1,10 @@
+import * as actionTypes from "./actions";
+
+const initialState = {
+  ingrediants: null,
+  totalPrice: 4,
+};
+
+const reducer = (state = initialState, action) => {};
+
+export default reducer;
